f9ebe9ea3bb3f253507aac2e476c554e31f52336
laboratorio <laboratorio@UPEAULA-31715>
2024-10-28 09:56:39 -0500

    Creacion de atrubutos de la clase AQauto

---
+++ Models/AQAuto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmableQuishpe_Examen1P.Models
+{
+    public class AQAuto
+    {
+
+        [Key]
+        public int AQAutoID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? AQMarca { get; set; }
+        
+        public int AQAnio { get; set; }
+
+        public string? AQCombustible { get; set; }
+
+        public bool AQUsado { get; set; }
+
+        [Range(0.01, 9999)]
+        public decimal AQPrecio { get; set; }
+
+        [EmailAddress]
+        public string? AQEmaildistribuidor { get; set; }
+
+        public DateTime AQFechaIngreso { get; set; }
+
+    }
+}
